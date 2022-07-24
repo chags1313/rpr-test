723afad4fe6790ca8eea9130ff69da92d0df8b8c
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2022-07-23 21:01:40 -0500

    Update rpr.py

--- rpr.py
+++ rpr.py
@@ -7,6 +7,22 @@ Created on Thu May 19 15:15:40 2022
 """
 
 
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 23 21:39:13 2022
+
+@author: chags
+"""
+
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 19 15:15:40 2022
+
+@author: chags
+"""
+
+
 import streamlit as st
 from io import StringIO
 import pandas as pd
@@ -30,32 +46,38 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.sidebar.title("Rapid Profile Rheometer 🩸")
 filen = 0
 com = pd.DataFrame()
+multiple_files = st.file_uploader(
+    "Multiple File Uploader",
+    accept_multiple_files=True
+)
+for file in multiple_files:
+    file_container = st.beta_expander(
+        f"File name: {file.name} ({file.size})")
+    data = StringIO.BytesIO(file.getbuffer())
+    file_container.write(pd.read_csv(data))
+
+st.write("### Code")
 uploaded_file = st.sidebar.file_uploader("Upload Your RPR Test File", accept_multiple_files=True)
+filen = 50
 if uploaded_file is not None:
             filen = + 1
             # To read file as bytes:
-            bytes_data = uploaded_file.getvalue()
+            for file in uploaded_file:
             #st.write(bytes_data)
 
-         # To convert to a string based IO:
-            stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-             #st.write(stringio)
+             # To convert to a string based IO:
+                 data = StringIO.BytesIO(file.getbuffer())
 
-         # To read file as string:
-            string_data = stringio.read()
-     #st.write(string_data)
-
-     # Can be used wherever a "file-like" object is accepted:
-            dataframe = pd.read_csv(uploaded_file)
+                 dataframe = pd.read_csv(data)
             dataframe['Date and Time of Test'] = str(dataframe['Datetime'].iloc[0])
             st.write(dataframe)
-            if filen > 1:
-                        com = pd.concat([dataframe, com], axis=0, join='inner')
-                        rrfdate = px.box(com, x= 'Datetime', y = 'Relative Resistance to Flow', color_discrete_sequence=['purple'])
-                        st.plotly_chart(rrfdate)
-                        sheardate = px.box(com, x= 'Datetime', y = 'Shear Rate', color_discrete_sequence=['green'])
-                        st.plotly_chart(sheardate)
-                        com['Flow Time in Seconds'] = com['Blood Sample']
-                        comp = px.scatter(com, x='mmHg range', y = 'Flow Time', color = 'Date and Time of Test')
-                        st.plotly_chart(comp)
+            com = dataframe
+            #com = pd.concat([dataframe, com], axis=0, join='inner')
+            rrfdate = px.box(com, x= 'Datetime', y = 'Relative Resistance to Flow', color_discrete_sequence=['purple'])
+            st.plotly_chart(rrfdate)
+            sheardate = px.box(com, x= 'Datetime', y = 'Shear Rate', color_discrete_sequence=['green'])
+            st.plotly_chart(sheardate)
+            com['Flow Time in Seconds'] = com['Blood Sample']
+            comp = px.scatter(com, x='mmHg range', y = 'Flow Time', color = 'Date and Time of Test')
+            st.plotly_chart(comp)
                         
