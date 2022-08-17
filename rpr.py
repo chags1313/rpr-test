fdf45b61f84417ee1e3d34b31fadcd8d9ed6c45f
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2022-08-17 16:58:44 -0500

    Update rpr.py

--- rpr.py
+++ rpr.py
@@ -230,6 +230,7 @@ with tab2:
         with c4:
             st.text("200 -s Shear Rate RRF")
             o = rrf[rrf['Shear Rate'] > 200]
+            o = rrf[rrf['Shear Rate'] < 210]
             o1 = o['Relative Resistance to Flow'].tail(5).mean()
             o1 = round(o1, 2)
             st.info(str(o1)) 
