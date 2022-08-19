7c6de7930056295d239d33352911a18ab4414164
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2022-08-19 11:21:51 -0500

    Update rpr.py

--- rpr.py
+++ rpr.py
@@ -150,11 +150,11 @@ with tab2:
                  first = avg_curve1['Amplitude - Normalized Pressure Data'].iloc[i] 
                  last = avg_curve1['Amplitude - Normalized Pressure Data'].iloc[-1] 
                  curve =  first - last
-                if needlesize == 15:
+                 if needlesize == 15:
                     Q = ((((0.6 * curve) / needlesize) / time)) * (1*10**-6)
-                if needlesize == 16:
+                 if needlesize == 16:
                     Q = ((((0.6 * curve) / needlesize) / time)) * (1*10**-10)
-                if needlesize == 20:
+                 if needlesize == 20:
                     Q = ((((0.6 * curve) / needlesize) / time)) * (1*10**-11)
                  shear = 4*(Q/(pi*(R**3)))
                  print(shear)
