09d821c81bf920d729f340fe62a13def59a73aaf
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2022-08-26 11:33:06 -0500

    Update rpr.py

--- rpr.py
+++ rpr.py
@@ -298,8 +298,8 @@ with tab3:
         
         shears = px.scatter(rrf, x='Shear Rate', y='Relative Resistance to Flow', color_discrete_sequence=['orange'],hover_data=["mmHg range", "Blood Sample", "Water Control"])
         #shear.data = [t for t in shears.data if t.mode == "lines"] trendline="lowess", trendline_options=dict(frac=0.09)
-        shears.update_yaxes(range=(0,100))
-        shears.update_xaxes(range=(0,200))
+        shears.update_yaxes(range=(0,200))
+        shears.update_xaxes(range=(0,500))
         shears.update_layout(width=525, hovermode='x unified')
         with e1:
             st.plotly_chart(shears, config= dict(
