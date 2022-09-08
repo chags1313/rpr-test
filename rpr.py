e8e5a2deb7b23ae5f58b6dab448fd67bcff13210
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2022-09-08 09:43:18 -0500

    Update rpr.py

--- rpr.py
+++ rpr.py
@@ -236,13 +236,13 @@ with tab4:
         shr = list()
         last_point = cur['Averaged Curve'].iloc[-1]
         cur['Averaged Curve'] = cur['Averaged Curve'] -  last_point
-        for numbers in reversed(np.arange(0.2, 50.1, 0.5)):
+        for numbers in reversed(np.arange(0.2, 50.1, 0.01)):
             u1 = cur[cur['Averaged Curve'] < numbers]
-            z1 = cur[cur['Averaged Curve'] < numbers - 0.1]
+            z1 = cur[cur['Averaged Curve'] < numbers - 0.01]
             bld.append((len(u1) - len(z1))/ 1000) 
             num.append(numbers)
             z = avg_curve1[avg_curve1['Amplitude - Normalized Pressure Data'] < numbers]
-            z = z[z['Amplitude - Normalized Pressure Data'] > (numbers - 0.5)]
+            z = z[z['Amplitude - Normalized Pressure Data'] > (numbers - 0.01)]
             shr.append((z['shear'].mean()))
         rrf = pd.DataFrame({'Blood Sample': bld})
         rrf['Water Control'] = np.full(shape=len(bld),fill_value=0.009,dtype=np.float) 
